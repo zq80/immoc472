b78004b4d7a848ef63fda1e858d3fcd99108c3ac
zeng qing <50618591@qq.com>
2025-05-20 17:01:00 +0800

    13-3 5月20号

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:4523/m1/6319140-6014427-default',
+  baseURL: 'http://192.168.3.26:4523/m1/6319140-6014427-default',
   timeout: 10000
 })
 
